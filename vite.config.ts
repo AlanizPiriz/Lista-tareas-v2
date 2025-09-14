a3b07f783303bcc3834c18a14fdbddca78d93b4f
Alan <alanizpiriz@gmail.com>
2025-09-13 22:54:51 -0300

    cambio vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // carpeta donde Vercel espera los archivos
+    emptyOutDir: true, // limpia la carpeta dist antes de build
+  },
+  base: './', // rutas relativas para que funcione en producción
 })
